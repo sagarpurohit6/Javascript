def07d53ba280d2c9ccdf562353abe8e28e6d12a
sagar purohit <98758502+sagarpurohit6@users.noreply.github.com>
2023-05-21 12:30:22 +0000

    conversion in datatypes

---
+++ 01_basic/04conversionoperation.js
@@ -0,0 +1,36 @@
+let number = undefined
+
+let stringToNumber = Number(number)
+console.log(typeof stringToNumber)
+console.log(stringToNumber)
+
+//number = "33" => 33 ,stringToNmber is number
+//number = "33abc"=>NaN, stringToNumber print NaN 
+//number = true => 1 , false=>0 , boolean value 
+//number = null => 0
+//number = undefined =>NaN
+//NOTE NaN typof is also number so wherever NaN comes it will give typeof number
+
+let isUserLoggedIn = undefined
+
+let boleeanIsUserLoggedIn = Boolean(isUserLoggedIn)
+console.log(typeof boleeanIsUserLoggedIn)
+console.log(boleeanIsUserLoggedIn)
+
+// 1 => true, 0=> false
+// ""=> false
+//"sagar"=> true
+//null=> false
+//undefined => false
+
+let number2 = null
+
+let numberToString = String(number2)
+console.log(typeof numberToString)
+console.log(numberToString)
+
+// 33 =>"33"
+
+
+
+
