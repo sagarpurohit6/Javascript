c9baa79a348ea062be8ec276c2e701c3245ea4a9
sagar purohit <98758502+sagarpurohit6@users.noreply.github.com>
2023-05-21 11:44:49 +0000

    datatypes in javascript

---
+++ 01_basic/03datatypes.js
@@ -0,0 +1,27 @@
+//datatypes in javascript
+//number => 1234
+//bigInt
+//string=>" "
+//boolean=> true/false
+//undefined
+//null => standalone value
+//symbol=> use for uniquness
+//object=>{}
+
+
+const number = 1234
+const string = "sagar"
+const boolean = true
+let undefinedValue;
+const value= null;
+const student={
+    name:"sagar",
+    rollNo:3
+}
+
+console.table([number,string,boolean,undefinedValue,value,student])
+console.log(typeof value)// null is type of object
+console.log(typeof undefinedValue)// undefined is typeof undefined
+console.log(typeof student)
+
+
