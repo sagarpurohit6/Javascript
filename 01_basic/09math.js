ea29f790b284bb3c984b9ec000c88aeb57d46fd3
sagar purohit <98758502+sagarpurohit6@users.noreply.github.com>
2023-05-28 12:00:21 +0000

    math in javascript

---
+++ 01_basic/09math.js
@@ -0,0 +1,17 @@
+console.log(Math.abs(-6))// convert negative value to positive
+console.log(Math.max(4,5,6,7));//give max value from number
+console.log(Math.min(4,5,6,7));// give minimum value
+console.log(Math.round(4.5));// give round of value if .5=> then give upper value
+console.log(Math.round(4.3))//give round of value if <5 then give lower value
+console.log(Math.ceil(4.1));// always give upper value 
+console.log(Math.floor(4.9));// always give lower value
+
+
+console.log(Math.random())//it gives random value between 0 and 1 
+
+console.log((Math.floor((Math.random()*10)+1)+10));//this formula always give value betwwen 10 to 20
+
+
+
+
+console.log((Math.floor((Math.random()*10)+1)+40));
