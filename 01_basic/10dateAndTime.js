ac662fb5c443791d0f555c97a91554d53ba0c192
sagar purohit <98758502+sagarpurohit6@users.noreply.github.com>
2023-05-29 20:04:22 +0000

    date and time

---
+++ 01_basic/10dateAndTime.js
@@ -0,0 +1,13 @@
+let date = new Date()
+
+// console.log(date.getDate())
+// console.log(date.toDateString());
+// console.log(date.toISOString());
+// console.log(date.toLocaleDateString());
+// console.log(date.toLocaleString());
+
+
+let dateDemo = new Date(2023,0,23)
+let dateDemoTwo = new Date(2023,3,25,5,3)
+console.log(dateDemoTwo.toLocaleTimeString());
+console.log(dateDemo.toLocaleDateString());
